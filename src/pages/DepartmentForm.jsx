873de3b95ec225ebd2f69ae130e87938b57e006f
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-04-26 15:41:30 -0300

    Alteração no título do form de departamentos

--- src/pages/DepartmentForm.jsx
+++ src/pages/DepartmentForm.jsx
@@ -2,20 +2,20 @@ export default function DepartmentForm() {
 
     return (
         <>
+            <h1>Cadastro de departamentos</h1>
             <form>
-                <h1>Cadastro de departamentos</h1>
-                    <div className="row">
-                        <div className="col-3">
-                            <label for="exempleInputId" className="form-label" >Id</label>
-                            <input type="text" className="form-control " id="disabledInput" placeholder="Seu ID" aria-label="ID Departamento" disabled/>
-                        </div>
-                        <div className="col">
-                            <label  for="exempleInputName" className="form-label">Nome</label>
-                            <input type="text" className="form-control" placeholder="Insira Seu Departamento" aria-label="Nome Departamento "/>
-                        </div>
+                <div className="row">
+                    <div className="col-3">
+                        <label for="exempleInputId" className="form-label" >Id</label>
+                        <input type="text" className="form-control " id="disabledInput" placeholder="Seu ID" aria-label="ID Departamento" disabled />
                     </div>
-                    <button  type="submit" className="btn btn-primary col-1 mt-2">Salvar</button>
-           </form>
+                    <div className="col">
+                        <label for="exempleInputName" className="form-label">Nome</label>
+                        <input type="text" className="form-control" placeholder="Insira Seu Departamento" aria-label="Nome Departamento " />
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary col-1 mt-2">Salvar</button>
+            </form>
         </>
     )
 }
