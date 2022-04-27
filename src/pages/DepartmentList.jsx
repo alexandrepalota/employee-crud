b5ffbef2d8d4deb0fcc65e95dcab9c58aea0f68e
Luiz Miranda <luizline@hotmail.com>
2022-04-27 19:41:48 -0300

    Lista departamento/funcionario e dropdown navibar

--- src/pages/DepartmentList.jsx
+++ src/pages/DepartmentList.jsx
@@ -2,7 +2,48 @@ export default function DepartmentList() {
 
     return (
         <>
-           <h1>lista de departamentos</h1>
+            <h1>lista de departamentos</h1>
+       
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col-3">Id</th>
+                        <th scope="col-3">Nome do Departamento</th>
+                        <th scope="col-3"> Editar/Excluir</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td className="idDepartamento"></td>
+                        <td className="departamento"></td>
+                        <td>
+                            <button type="submit" className=" border-dark btn btn-light mx-1">✏️</button> 
+                            <button type="submit" className=" border-dark btn btn-light   ">🗑️</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td className="idDepartamento"></td>
+                        <td className="departamento"></td>
+                        <td className="botaoDepartamento">
+                            <button type="submit" className=" border-dark btn btn-light mx-1">✏️</button> 
+                            <button type="submit" className=" border-dark btn btn-light   ">🗑️</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td className="idDepartamento"></td>
+                        <td className="departamento"></td>
+                        <td className="botaoDepartamento">
+                            <button type="submit" className=" border-dark btn btn-light mx-1">✏️</button> 
+                            <button type="submit" className=" border-dark btn btn-light   ">🗑️</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -3,6 +3,43 @@ export default function EmployeeList() {
     return (
         <>
             <h1>Lista de funcionários</h1>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col-3">Id</th>
+                        <th scope="col-3">Nome do Departamento</th>
+                        <th scope="col-3"> Data de Admissão</th>
+                        <th scope="col-3"> Data de Demissão</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td className="idFuncionario"></td >
+                        <td className="nomeFuncionario" >Gumercindo</td >
+                        <td className="admissao text-success " placeholder="xx/xx/xxxx" >Admitido em 11/11/2011</td >
+                        <td className="demissao text-danger"placeholder="xx/xx/xxxx" >Demitido em 10/10/2019</td >
+                        
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td className="idFuncionario"></td >
+                        <td className="nomeFuncionario" >Fulado de tal</td >
+                        <td className="admissao text-success" placeholder="xx/xx/xxxx" >Exercendo a função</td >
+                        <td className="demissao text-danger"  placeholder="xx/xx/xxxx">xx/xx/xxxx</td >
+                        
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td className="idFuncionario"></td >
+                        <td className="nomeFuncionario" ></td >
+                        <td className="admissao text-success" placeholder="xx/xx/xxxx" ></td >
+                        <td className="demissao text-danger"  placeholder="xx/xx/xxxx"></td >
+                       
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
